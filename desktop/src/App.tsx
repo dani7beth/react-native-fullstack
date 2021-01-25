44c50f73f7bb3f561b769f3794b355c033405eda
dani7beth <danielledona28@gmail.com>
2021-01-25 12:47:32 -0700

    added styling to mobile app

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,54 +1,4 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import icon from '../assets/icon.svg';
-
-// const Hello = () => {
-//   return (
-//     <div>
-//       <div className="Hello">
-//         <img width="200px" alt="icon" src={icon} />
-//       </div>
-//       <h1>electron-react-boilerplate</h1>
-//       <div className="Hello">
-//         <a
-//           href="https://electron-react-boilerplate.js.org/"
-//           target="_blank"
-//           rel="noreferrer"
-//         >
-//           <button type="button">
-//             <span role="img" aria-label="books">
-//               📚
-//             </span>
-//             Read our docs
-//           </button>
-//         </a>
-//         <a
-//           href="https://github.com/sponsors/electron-react-boilerplate"
-//           target="_blank"
-//           rel="noreferrer"
-//         >
-//           <button type="button">
-//             <span role="img" aria-label="books">
-//               🙏
-//             </span>
-//             Donate
-//           </button>
-//         </a>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default function App() {
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route path="/" component={Hello} />
-//       </Switch>
-//     </Router>
-//   );
-// }
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
@@ -74,7 +24,7 @@ function App() {
   const renderThings = ()=>{
     return things.map((thing)=>{
       return(
-        <div>
+        <div key={Math.random()}>
           <h3>{thing.name}</h3>
           <p>{thing.likes}</p>
         </div>
--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, StyleSheet, StatusBar, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import axios from "axios";
 
 import {
@@ -30,20 +39,45 @@ const App = () => {
       setLoading(false);
     }
   };
+
+  const likeClicked = async (id) => {
+    try {
+      let res = await axios.put(`http://localhost:3001/api/like/${id}`);
+      let updatedthings = things.map((thing) =>
+        thing.id == id ? res.data : thing
+      );
+      setThings(updatedthings.sort((a, b) => b.likes - a.likes));
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const renderThings = () => {
     return things.map((thing) => {
       return (
-        <View>
-          <Text>{thing.name}</Text>
-          <Text>{thing.likes}</Text>
+        <View style={styles.card} key={thing.id}>
+          <View style={styles.cardheader}>
+            <Text style={styles.header} >{thing.name}</Text>
+            <Text style={styles.paragraph}>likes: {thing.likes}</Text>
+          </View>
+          <View>
+            <TouchableOpacity onPress={() => likeClicked(thing.id)} style={styles.button}>
+              <Text>Like</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       );
     });
   };
   const renderContent = () => {
-    if (loading) return <Text>loading</Text>;
+    if (loading)
+      return (
+        <>
+          <ActivityIndicator size="large" color="#00ff00" />
+          <Text style={styles.header}>loading</Text>
+        </>
+      );
     if (error) return <Text>error occurred</Text>;
-    return renderThings();
+    return <ScrollView>{renderThings()}</ScrollView>;
   };
   return (
     <>
@@ -53,6 +87,43 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 34,
+    paddingVertical: 12,
+  },
+  paragraph: {
+    fontSize: 25,
+    padding: 10,
+  },
+  card: {
+    flex: 1,
+    justifyContent: "space-between",
+    flexDirection: 'column',
+    marginVertical: 5,
+    marginHorizontal: 10,
+    borderWidth: 2,
+    borderColor: "#ccc",
+    minHeight: 200,
+  },
+  cardheader: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    padding: 14,
+    shadowColor: '#ddd',
+    shadowOffset: {width: 3, height: 3},
+    shadowRadius: 3,
+    shadowOpacity: 5,
+    backgroundColor: 'white',
+    margin: 10
+  }
+
+});
 
 export default App;
